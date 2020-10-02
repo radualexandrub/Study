5c5b1ce3fc85106b816ba9f27e4663882d616ba1
Radu-Alexandru Bulai <radualexandrubulai@gmail.com>
2020-10-02 18:16:39 +0300

    Add React ToDo App

---
+++ React/todo/precache-manifest.c37aef893bb9617f828b043550a6bc4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "813c198e0c5ee9d64de2af317444151f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2a3f41738a2591f2b090",
+    "url": "./static/css/main.14583d7b.chunk.css"
+  },
+  {
+    "revision": "b7b3c787e0542a80a5f8",
+    "url": "./static/js/2.ab8f88eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ab8f88eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a3f41738a2591f2b090",
+    "url": "./static/js/main.19536771.chunk.js"
+  },
+  {
+    "revision": "f2886ae4f32e95d0a917",
+    "url": "./static/js/runtime-main.b4c0ae84.js"
+  }
+]);
